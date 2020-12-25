bee3689ae92eb86058a02dc192e0a6c5838ca839
Alexander Bazo <alexanderbazo@googlemail.com>
2020-12-26 00:26:57 +0100

    Add spawning enemies to game scene

--- src/game/scenes/game/GameScene.java
+++ src/game/scenes/game/GameScene.java
@@ -1,23 +1,52 @@
 package game.scenes.game;
 
 import config.ColorScheme;
-import config.GameConfig;
+import config.Display;
+import de.ur.mi.oop.graphics.Point;
+import game.actors.Enemy;
 import game.actors.Player;
+import game.levels.Level;
 import game.scenes.BaseScene;
 import game.scenes.SceneListener;
 import game.scenes.SceneType;
 
-public class GameScene extends BaseScene {
+import java.util.ArrayList;
+
+public class GameScene extends BaseScene implements EnemySpawnListener {
 
     public static final String SCENE_NAME = "Game Scene";
 
+    private Level level;
     private Player player;
+    private ArrayList<Enemy> enemies;
+    private EnemySpawner spawner;
 
     public GameScene(SceneListener sceneListener) {
         super(SCENE_NAME, SceneType.GAME, sceneListener);
         setBackgroundColor(ColorScheme.LINEN);
-        player = new Player(GameConfig.WINDOW_WIDTH/2, GameConfig.WINDOW_HEIGHT/2, this);
+        level = Level.next();
+        player = new Player(Display.WINDOW_WIDTH / 2, Display.WINDOW_HEIGHT / 2, level.playerSpeed, this);
+        enemies = new ArrayList<>();
+        spawner = new EnemySpawner(this, this);
+        spawner.setLevel(level);
         addActor(player);
     }
 
+    @Override
+    public void update() {
+        super.update();
+        if (enemies.size() < level.totalNumberOfEnemies) {
+            spawner.update();
+        }
+    }
+
+    public Point getPlayerPosition() {
+        return new Point(player.getPosition().getXPos(), player.getPosition().getYPos());
+    }
+
+    @Override
+    public void onEnemySpawned(Enemy enemy) {
+        enemies.add(enemy);
+        addActor(enemy);
+    }
 }
