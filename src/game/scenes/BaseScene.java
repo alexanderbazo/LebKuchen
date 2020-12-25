d9f8381de48d4bd7c6156a5284b36cc8e79cadec
Alexander Bazo <alexanderbazo@googlemail.com>
2020-12-25 19:20:26 +0100

    Add FontLoader for custom fonts

--- src/game/scenes/BaseScene.java
+++ src/game/scenes/BaseScene.java
@@ -1,37 +1,45 @@
 package game.scenes;
 
 import config.GameConfig;
-import de.ur.mi.oop.app.GraphicsApp;
 import de.ur.mi.oop.colors.Color;
+import de.ur.mi.oop.colors.Colors;
+import de.ur.mi.oop.graphics.GraphicsObject;
+import de.ur.mi.oop.graphics.Image;
 import de.ur.mi.oop.graphics.Rectangle;
 
 import java.util.ArrayList;
 
 public abstract class BaseScene implements InputHandler {
 
-    private final GraphicsApp app;
     private final String sceneName;
+    private final SceneType sceneType;
+    private final SceneListener sceneListener;
     private final ArrayList<Actor> actors;
-    private final Rectangle background;
+    private GraphicsObject background;
     private SceneState sceneState;
 
-    public BaseScene(GraphicsApp app, String sceneName, Color backgroundColor) {
-        this.app = app;
+    public BaseScene(String sceneName, SceneType sceneType, SceneListener sceneListener) {
         this.sceneName = sceneName;
-        this.background = new Rectangle(0, 0, GameConfig.WINDOW_WIDTH,GameConfig.WINDOW_HEIGHT, backgroundColor);
+        this.sceneType = sceneType;
+        this.sceneListener = sceneListener;
         this.actors = new ArrayList<>();
+        this.background = new Rectangle(0, 0, GameConfig.WINDOW_WIDTH, GameConfig.WINDOW_HEIGHT, Colors.BLACK);
+    }
+
+    public BaseScene(String sceneName, SceneType sceneType, SceneListener sceneListener, String backgroundImagePath) {
+        this(sceneName, sceneType, sceneListener);
+        Image image = new Image(0, 0, backgroundImagePath);
+        image.setWidth(GameConfig.WINDOW_WIDTH, true);
+        image.setHeight(GameConfig.WINDOW_HEIGHT, true);
+        this.background = image;
     }
 
     public String getName() {
         return sceneName;
     }
 
-    public void addActor(Actor actor) {
-        actors.add(actor);
-    }
-
-    public void removeActor(Actor actor) {
-        actors.remove(actor);
+    public SceneType getType() {
+        return sceneType;
     }
 
     public Color getBackgroundColor() {
@@ -42,12 +50,22 @@ public abstract class BaseScene implements InputHandler {
         this.background.setColor(backgroundColor);
     }
 
+    public void addActor(Actor actor) {
+        actors.add(actor);
+    }
+
+    public void removeActor(Actor actor) {
+        actors.remove(actor);
+    }
+
     public void play() {
         sceneState = SceneState.PLAYING;
+        sceneListener.onScenePlayed(this);
     }
 
     public void pause() {
         sceneState = SceneState.PAUSED;
+        sceneListener.onScenePaused(this);
     }
 
     public SceneState getState() {
@@ -56,9 +74,20 @@ public abstract class BaseScene implements InputHandler {
 
     public void render() {
         background.draw();
+        update();
+        draw();
+    }
+
+    private void update() {
+        if (sceneState == SceneState.PAUSED) {
+            return;
+        }
         for (Actor actor : actors) {
             actor.update();
         }
+    }
+
+    private void draw() {
         for (Actor actor : actors) {
             actor.draw();
         }
---
+++ src/utils/FontLoader.java
@@ -0,0 +1,82 @@
+package utils;
+
+import java.awt.*;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+
+/**
+ * Diese Klasse unterstützt Sie bei der Integration zusätzlicher Schriftarten in Ihre GraphicsApp-Umgebung. Verwenden Sie die
+ * Methode "loadFonts", um zu Beginn zusätzliche TrueType-Schriftarten aus einem Verzeichnis innerhalb des Projekts zu laden.
+ * Diese Schriftarten können im Anschluss in Label-Instanzen verwendet werden.
+ *
+ * Beispiel:
+ *
+ *  // z.B. in initialize()
+ *  FontHelper.loadFonts("data/fonts");
+ *
+ *  // Verwenden der geladenen Schriftarten
+ *  Label label = new Label(0, 0, "Hello World");
+ *  label.setFont("CustomFont"); // Hier muss der FontName der zusätzlich geladenen Schriftart angegeben werden
+ *
+ * Eine Liste der per "loadFonts" geladenen Schriftarten können Sie über den Aufruf der Methode "printLoadedFonts()"
+ * auf der Konsole ausgeben:
+ *
+ *   FontHelper.printLoadedFonts();
+ */
+
+public abstract class FontLoader {
+
+    private static final ArrayList<Font> customFontsLoaded = new ArrayList<>();
+
+    /**
+     * Versucht, alle TrueType-Schriftarten (Identifikation über die Dateiendung ttf) aus dem angegebenen Ordner zu laden und
+     * in der laufenden Anwendung bereitzustellen.
+     *
+     * @param pathToFonts Pfad zum Ordner mit den neuen Schriftarten
+     */
+    public static void loadFonts(String pathToFonts) {
+        File fontDir = new File(pathToFonts);
+        File[] fontFiles = fontDir.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".ttf");
+            }
+        });
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        for (File fontFile : fontFiles) {
+            try {
+                Font font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+                ge.registerFont(font);
+                customFontsLoaded.add(font);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Gibt true zurück, wenn die Schriftart mit dem angegebenen FontName in der laufenden Anwendung zur Verfügung steht.
+     *
+     * @param name FontName der zu prüfenden Schriftart
+     * @return true, wenn Schriftart verwendet werden kann
+     */
+    public static boolean checkIfFontIsLoaded(String name) {
+        for (Font customFont : customFontsLoaded) {
+            if (customFont.getFontName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Gibt eine Liste aller über diese Klasse geladenen, zusätzlichen Schriftarten auf der Konsole aus.
+     */
+    public static void printLoadedFonts() {
+        System.out.println("### Available Fonts (FontName & Family) ###");
+        for (int i = 0; i < customFontsLoaded.size(); i++) {
+            System.out.println("#" + i + "\t" + customFontsLoaded.get(i).getFontName() + " (" + customFontsLoaded.get(i).getFamily() + ")");
+        }
+    }
+}
