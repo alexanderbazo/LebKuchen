5111900bcf0a0e2ead3c630bc1afe521059a8dc2
Alexander Bazo <alexanderbazo@googlemail.com>
2020-12-26 00:18:42 +0100

    Refactor config files

--- src/config/Display.java
+++ src/config/Display.java
@@ -2,9 +2,7 @@ package config;
 
 import de.ur.mi.oop.colors.Color;
 
-public abstract class GameConfig {
-
-    public static final String GAME_VERSION = "0.0.1";
+public abstract class Display {
 
     public static final int WINDOW_WIDTH = 640;
     public static final int WINDOW_HEIGHT = 480;
---
+++ src/config/Game.java
@@ -0,0 +1,7 @@
+package config;
+
+public class Game {
+
+    public static final String GAME_VERSION = "0.0.1";
+    
+}
